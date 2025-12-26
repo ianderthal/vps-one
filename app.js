d6232d7784fe2541cae145cbf8b36d5b3fc352d6
Ian Gallardo <ian@iangallardo.com>
2025-12-26 10:03:05 -0800

    add inline html

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const http = require('http');
 
 http.createServer(function (req, res) {
-res.write("Silence is golden.");
-res.end();
-
-
-}
-).listen(3001);
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>My Page</title>
+    </head>
+    <body>
+      <h1>Silence is golden.</h1>
+      <p>Welcome to my page!</p>
+    </body>
+    </html>
+  `);
+  res.end();
+}).listen(3001);
 
 console.log("Server started on port 3001");
